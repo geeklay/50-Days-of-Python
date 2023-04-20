945aea02ddfac1dccffcd5755cfcfcf7d828c059
geeklay <kenjuge@gmail.com>
2023-04-20 00:12:21 -0700

    Added extra challenge: tuple of student sex

--- Day1/5.py
+++ Day1/5.py
@@ -5,4 +5,16 @@ def mydiscount():
     discount = input()
     print(price - (price * int(discount[:discount.index('%')]) / 100))
 
+def students(l):
+    k = [i.casefold() for i in l ]
+    m = k.count("male")
+    f = k.count("female") 
+    return [("Male", m), ("Female", f)]
+
+
+
 mydiscount()
+ 
+a = students(['Male', 'Female', 'male', 'male', 'male', 'female', 'male',
+          'Female', 'Male', 'Female', 'Male', 'female'])
+print(a)
